b942cadd4c469cb721f99da185a34d11b80a8b51
Zach Levine <zachary.levine@gmail.com>
2017-05-13 15:20:55 -0400

    Adjust headings

--- client/src/Post.js
+++ client/src/Post.js
@@ -19,10 +19,12 @@ class Post extends Component {
 			<div className="eleven wide column">
 				<span className="ui orange ribbon label">Newest Post {id}</span>
 				<div className="ui text container">
-					<h1 className="ui header">
+					<h1>Analyzing Post {id}</h1>
+					<div className="ui divider"></div>
+					<h2 className="ui header">
 						{title} <div className="ui red horizontal label">{lang}</div>
 						<div className="sub header header__link">{url}</div>
-					</h1>
+					</h2>
 					<div className="ui divider"></div>
 					<div className="post__content post__content--truncated">
 						{content}
