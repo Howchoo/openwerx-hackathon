2700fc618dead1e475ac0472f72f2eec263b8e14
Mason Jeffers <MasonJeffers@gmail.com>
2017-05-13 02:04:39 -0400

    now it's a python dictionary

--- rssAnalyzer/rssAnalyzer.py
+++ rssAnalyzer/rssAnalyzer.py
@@ -3,10 +3,11 @@ import json
 from bs4 import BeautifulSoup
 
 def pull_latest(feed_URL, howManyFeedsToPull ):
+	latestFeed = {}
 	for i in range (howManyFeedsToPull):
 		feed = feedparser.parse( feed_URL )
-		soup = BeautifulSoup(feed['entries'][0]['summary_detail']['value'], "lxml")  #this makes soup be the latest single feed in streem
-	return soup
+		latestFeed[i] = BeautifulSoup(feed['entries'][0]['summary_detail']['value'], "lxml")  #this makes soup be the latest single feed in streem
+	return latestFeed
 
 #TO USE
 #from rssAnalyzer import *
