1a6bf56da5106c05901764a172bd0dbb420b3070
Jeremy Smith <jesmith4@mail.usf.edu>
2017-05-13 03:33:05 -0400

    get range entries

--- greypill/rssfeedinterface.py
+++ greypill/rssfeedinterface.py
@@ -18,14 +18,30 @@ class RssFeedInterface():
     def get_full_feed(self):
         return self.feed
     
+    def get_feed_entries(self):
+        return self.get_full_feed()['entries']
+    
     def get_feed_length(self):
-        return len(self.get_full_feed()['entries'])
+        return len(self.get_feed_entries())
+    
+    def get_feed_titles(self):
+        return map(lambda x: self.get_feed_entries()[x]['title'], range(self.get_feed_length()))
     
     def get_single_entry(self, index):
         if (index <= self.get_feed_length()):
-            return self.get_full_feed()['entries'][index]
+            return self.get_feed_entries()[index]
         else:
             print 'Index out of range of feed entires'
+            return None
+        
+    def get_range_entries(self, start, stop):
+        """definitely a bug if not given a valid range"""
+        feedlen = self.get_feed_length()
+        if (start <= stop):
+            if (start >= 0 and start < feedlen and stop < feedlen):
+                return map(lambda x: self.get_feed_entries()[x], range(start, stop+1))
+        else:
+            print 'Start index must be greater than or equal to stop index'
             return None
     
     def refresh_feed(self):
@@ -36,6 +52,5 @@ class RssFeedInterface():
 if __name__ == '__main__':
     feed = RssFeedInterface('https://streemian.com/rss/')
     print feed.get_single_entry(0)['title']
-    print feed.get_single_entry(1)['title']
-    feed.refresh_feed()
-    print feed.get_single_entry(0)['title']
+    print feed.get_feed_titles()
+    print len(feed.get_range_entries(0,9))
