fac0ed69d5286ad58466c027d47978e2b1127d86
Jeremy Smith <jesmith4@mail.usf.edu>
2017-05-13 12:14:56 -0400

    translation added

--- greypill/server.py
+++ greypill/server.py
@@ -1,5 +1,8 @@
 import rssfeedinterface
 import sentimentanalyzer
+import json
+import requests
+import util
 from BeautifulSoup import BeautifulSoup
 
 from flask import Flask
@@ -55,35 +58,48 @@ def handle_mastodon_feed():
     bucket_index = get_bucket_index()
     
     if request.method == 'POST':
-        with open('temp/bucket.'+str(bucket_index), 'w+') as f:
-            f.write(str(request.get_json()))
+        with open('temp/bucket.'+str(bucket_index)+'.json', 'w+') as f:
+            f.write(json.dumps(request.get_json()))
         increment_bucket()
         return '200'
     else:
         data = []
-        
-        for x in range(bucket_size):
-            try:
-                with open('temp/bucket.'+str(bucket_index), 'r') as f:
-                    data.append(f.readlines())
-            except Exception as e:
-                raise e
+        for x in range(bucket_size):   
+            with open('temp/bucket.'+str(bucket_index)+'.json', 'r') as f:
+                status = json.loads(f.read())
+                
+            cleaned_status = BeautifulSoup(status['account']['note']).text
+            jsondata = [{
+                'data_source': 'mastodon',
+                'created_at': status['created_at'],
+                'title': status['account']['username'],
+                'summary_detail': cleaned_status,
+                'sentiment': analyzer.getSentiment(cleaned_status)
+            }]
+            data.append(jsondata)
                 
         return jsonify(data)
 
 def construct_json(entry):
     
-    data = []
-    
     cleaned_summary = BeautifulSoup(entry['summary_detail']['value']).text
+    translation_data = translate(cleaned_summary)['data']
+    translated_summary = translation_data['translations'][0]['translatedText']
+    detected_language = translation_data['translations'][0]['detectedSourceLanguage']
     
-    json = [{
+    print translated_summary
+    print detected_language
+    
+    jsondata = [{
+        'data_source': 'streemit',
+        'created_at': entry['published'],
         'title': entry['title'],
-        'summary_detail': cleaned_summary,
-        'sentiment': analyzer.getSentiment(cleaned_summary)
+        'summary_detail': translated_summary,
+        'source_language': detected_language,
+        'sentiment': analyzer.getSentiment(translated_summary)
     }]
     
-    return json
+    return jsondata
 
 def increment_bucket():
     
@@ -104,6 +120,11 @@ def get_bucket_index():
     with open('temp/bucketindex.ddd', 'r') as f:
         bucket_index = int(f.read())
     return bucket_index
+
+def translate(paragraph, lang='en'):
+    key = util.get_config_value('googleTranslateApiKey')
+    r = requests.get('https://www.googleapis.com/language/translate/v2', params={'target': lang, 'key': key, 'q': paragraph, 'format': 'text'})
+    return r.json()
 
 if __name__ == '__main__':
     app.run(debug=True)
