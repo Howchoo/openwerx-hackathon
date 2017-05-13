bd67add5e9f5f5e4bd41bb8d7e57b977697a53eb
Zach Levine <zach.levine@BHOLT1B1602ZACH.fios-router.home>
2017-05-13 03:38:11 -0400

    Expand static example

---
+++ static/js/static.js
@@ -0,0 +1,36 @@
+/*
+ * Set up a sample chart
+ */
+var data = {
+    labels: [
+        "Positive",
+        "Neutral",
+        "Negative"
+    ],
+    datasets: [
+        {
+            data: [0.274, 0.095, 0.631],
+            backgroundColor: [
+                "#4CAF50",
+                "#FFC107",
+                "#F44336"
+            ],
+            hoverBackgroundColor: [
+                "#43A047",
+                "#FFB300",
+                "#E53935"
+            ]
+        }]
+};
+
+var ctx = document.getElementById('postSentiment');
+
+var postSentiment = new Chart(ctx,{
+    type: 'doughnut',
+    data: data,
+    options: {
+        legend: {
+            position: 'bottom'
+        }
+    }
+});
