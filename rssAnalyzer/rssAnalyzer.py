d3c602e1f2b426c60d3c7e675b2c1f9f2a77a746
Mason Jeffers <MasonJeffers@gmail.com>
2017-05-13 01:51:03 -0400

    fixed error

--- rssAnalyzer/rssAnalyzer.py
+++ rssAnalyzer/rssAnalyzer.py
@@ -5,10 +5,14 @@ from bs4 import BeautifulSoup
 def pull_latest(feed_URL, howManyFeedsToPull ):
 	for i in range (howManyFeedsToPull):
 		feed = feedparser.parse( feed_URL )
-		soup = BeautifulSoup(feed['entries'][0]['summary_detail']['value'])  #this makes soup be the latest single feed in streem
+		soup = BeautifulSoup(feed['entries'][0]['summary_detail']['value'], "lxml")  #this makes soup be the latest single feed in streem
 	return soup
+
 #TO USE
-#from rssAnalyzer.py import *
+#from rssAnalyzer import *
 #pull_latest('https://streemian.com/rss/',5)
 
+
 #This may only work on https://streemian.com/rss/
+
+
